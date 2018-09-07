88ff8e2feaa25542673f18db5258e6a25f021254
BdonGordon <bgordon@linamar.com>
2018-09-07 13:44:07 -0400

    - Added the get and JsonResponse of cars json object

--- FancyCars-API/fancycars/views.py
+++ FancyCars-API/fancycars/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from rest_framework.views import APIView
 
 # Create your views here.
@@ -7,6 +7,32 @@ from rest_framework.views import APIView
 def index(request):
     return HttpResponse("Hello")
 
+#http://127.0.0.1:8000/fancycars/cars
+
 class CarsService(APIView):
     def get(self, format=None):
-        return HttpResponse("CarsService")
+        cars = []
+        carOne = CreateCars(1, "img1", "name1", "make1", "model1", 1990, "Available")
+        carTwo = CreateCars(2, "img2", "name2", "make2", "model2", 1999, "Out of Stock")
+
+        cars.append(carOne.createCarObject())
+        cars.append(carTwo.createCarObject())
+
+        return JsonResponse({'cars': cars})
+
+
+class CreateCars:
+    def __init__(self, id, img, name, make, model, year, available):
+        self.id = id
+        self.img = img
+        self.name = name
+        self.make = make
+        self.model = model
+        self.year = year
+        self.available = available
+
+    def createCarObject(self):
+        car = {'id': self.id, 'img': self.img, 'name': self.name, 'make': self.make, 'model': self.model, 'year': self.year, 'available': self.available}
+
+        return car
+

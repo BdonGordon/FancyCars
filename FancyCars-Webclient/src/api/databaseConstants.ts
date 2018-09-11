6b08963c66efa276f271e45c148a86a2338c1975
BdonGordon <bgordon@linamar.com>
2018-09-11 11:19:27 -0400

    - Added new package.json scripts for API

--- FancyCars-Webclient/src/api/databaseConstants.ts
+++ FancyCars-Webclient/src/api/databaseConstants.ts
@@ -1,3 +1,3 @@
-//export const CARS_SERVICE_URL: string = 'http://10.201.49.24:8000/fancycars/cars';
+export const CARS_SERVICE_URL: string = 'http://10.201.49.24:8000/fancycars/cars';
 //export const CARS_SERVICE_URL: string = 'http://192.168.0.16:8000/fancycars/cars';
-export const CARS_SERVICE_URL: string = 'http://10.193.78.124:8000/fancycars/cars';
+//export const CARS_SERVICE_URL: string = 'http://10.193.78.124:8000/fancycars/cars';

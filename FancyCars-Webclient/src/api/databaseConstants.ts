a472616955140be20eb4eadffdeb7e4274f06961
BdonGordon <brandon-g32@hotmail.com>
2018-09-12 22:25:55 -0400

    - commented out local ips

--- FancyCars-Webclient/src/api/databaseConstants.ts
+++ FancyCars-Webclient/src/api/databaseConstants.ts
@@ -3,6 +3,6 @@
 //export const AVAILABILITY_URL: string = 'http://XX.XXX.XX.XX:8000/fancycars/availability/id=';
 
 
-/**Ignore ips below**/
-export const AVAILABILITY_URL: string = 'http://192.168.0.16:8000/fancycars/availability/id=';
-export const CARS_SERVICE_URL: string = 'http://192.168.0.16:8000/fancycars/cars';
+//**Ignore ips below**/
+//export const AVAILABILITY_URL: string = 'http://192.168.0.16:8000/fancycars/availability/id=';
+//export const CARS_SERVICE_URL: string = 'http://192.168.0.16:8000/fancycars/cars';
